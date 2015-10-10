3b0cadba788e21f4906ef2e9b5a9d7e5ff0facb7
Max PRUDHOMME <max.prudhomme@gmail.com>
2015-10-10 19:14:25 +0530

    uikit added navbar design done

--- django-uikit-admin/settings.py
+++ django-uikit-admin/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django-uikit-admin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +69,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'django-uikit-admin.wsgi.application'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Database
