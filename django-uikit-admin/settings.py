7400ddb97b4fe15d44b1e8f95630a79ee04ff072
Max PRUDHOMME <max.prudhomme@gmail.com>
2015-11-03 22:56:42 +0530

    layout redone (sidebar removed)

--- django-uikit-admin/settings.py
+++ django-uikit-admin/settings.py
@@ -57,6 +57,7 @@ ROOT_URLCONF = 'django-uikit-admin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # 'DIRS': [],
         'DIRS': [os.path.join(BASE_DIR, 'uikit_admin/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
