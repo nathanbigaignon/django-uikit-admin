69fa616f510adb27254a1664d97bfeaf200f0d5a
Max PRUDHOMME <max.prudhomme@gmail.com>
2015-11-08 14:06:03 +0530

    Added buttons for date and time completion All the input fields have the same length

--- uikit_admin/templatetags/uikit_admin.py
+++ uikit_admin/templatetags/uikit_admin.py
@@ -7,14 +7,16 @@ register = template.Library()
 
 @register.simple_tag
 def uka_form_row_stacked(element, errors='', classes=''):
+    class_margin_top = ''
     label = BoundField.label_tag(element, "", {'class': 'uk-form-label'})
     if errors:
         classes_tmp = classes + ' uk-form-danger'
         classes = classes_tmp
-    element = element.as_widget(attrs={'class': classes})
-    html_error = format_html('<div class="uk-text-danger uk-margin-top">{}</div>', errors)
+        class_margin_top = 'uk-margin-top'
+    element = element.as_widget(attrs={'class': classes})  # to remove (be done in js)
+    html_error = format_html('<div class="uk-text-danger {}">{}</div>', class_margin_top, errors)
     html = format_html(
-        '<div class="uk-form-row">{}<div class="uk-form-row">{}</div>{}</div>', label, element, html_error)
+        '<div class="uk-form-row">{}<div class="uk-form-controls">{}</div>{}</div>', label, element, html_error)
     return html
 
 
@@ -23,7 +25,7 @@ def uka_form_row_stacked_button(text, classes=None):
     if classes is None:
         classes = ''
     html = format_html(
-        '<div class="uk-form-row"><div class="uk-form-row"><button class="uk-button {}">{}</button></div></div>',
+        '<div class="uk-form-row"><div class="uk-form-controls"><button class="uk-button {}">{}</button></div></div>',
         classes, text)
     return html
 
@@ -40,3 +42,8 @@ def uka_button(text, classes=None, type=None, name=None):
         '<button class="uk-button {}" type="{}" name="{}">{}</button>',
         classes, type, name, text)
     return html
+
+
+@register.filter(name='addcss')
+def addcss(field, css):
+    return field.as_widget(attrs={"class": css})
