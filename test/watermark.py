0c63d6d17559fef3611a5cb1bbd6c747aa6ec9cd
Nathan BIGAIGNON <nathanbigaignon@gmail.com>
2015-12-23 01:29:13 +0100

    test folder deleted

--- test/watermark.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageFilter
-try:
-    original = Image.open("test.png")
-    print("The size of the Image is: ")
-    print(original.format, original.size, original.mode)
-    blurred = original.filter(ImageFilter.BLUR)
-    blurred.save('blurred.png')
-except:
-    print("Unable to load image")
